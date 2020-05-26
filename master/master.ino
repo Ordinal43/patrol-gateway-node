79ffc929972038153b3ce549d97f5bed048aafad
Bryan Ramaputra <bryan.ramaputra@ti.ukdw.ac.id>
2020-05-26 11:50:01 +0700

    Added master and slave code

---
+++ master/master.ino
@@ -0,0 +1,334 @@
+/*********************
+  | WIFI ESP32
+*********************/
+#include <WiFi.h>
+//const char ssid[] = "kingcrimson";
+//const char pass[] = "lalilulelo";
+WiFiClient net;
+
+/*********************
+  | MQTT CLIENT
+*********************/
+#include <MQTT.h>
+#include <MQTTClient.h>
+MQTTClient client;
+String LIST_NODENAME_TOPIC;
+String LIST_QR_TOPIC = "/qrcode/";
+
+/*********************
+  | EEPROM
+*********************/
+#include "EEPROM.h"
+#define EEPROM_SIZE 128
+String strSSID;
+String strPASS;
+String strBrokerAdd;
+String usernameMQTT;
+String passwordMQTT;
+String gatewayName;
+
+char buff1[10];
+char buff2[25];
+char buff3[50];
+char buff4[150];
+
+/*********************
+  | nRF24L01+
+*********************/
+#include <SPI.h>
+#include <RF24.h>
+#include <printf.h>
+
+// set Chip-Enable (CE) and Chip-Select-Not (CSN) radio setup pins
+#define CE_PIN 17
+#define CSN_PIN 4
+
+// set transmission cycle send rate - in milliseconds
+#define SEND_RATE 1000
+
+// create RF24 radio object using selected CE and CSN pins
+RF24 radio(CE_PIN,CSN_PIN);
+
+#define MAX_NRF_CONNECTION 100
+
+// counter for address amount
+int addrAmount;
+
+struct payload {
+   String message;
+   int messageCount;
+};
+
+typedef struct payload Payload;
+
+// String array to store addresses for each sensor node
+// used to find index of remoteNodeData array
+String strArrNodeAddr[MAX_NRF_CONNECTION];
+
+// array of Payloads for data from each slave node: { message, returned_count }
+Payload remoteNodeData[MAX_NRF_CONNECTION];
+
+// system operation timing variables - set SEND_RATE to limit transmit rate
+unsigned long currentTime;
+unsigned long lastSentTime;
+
+#define LEDPIN 2
+
+/*********************
+  | METHOD DEFINITIONS
+*********************/
+String getValue(String data, char separator, int index);
+void mqttConnect();
+void mqttMessageReceived(String &topic, String &payload);
+void nrfConnect();
+void callAndReceiveNodeData(String targetQrNodeName, String payload);
+
+void setup() {
+    // setup serial communications for basic program display
+    Serial.begin(115200);
+    Serial.println("[*][*][*] Beginning MQTT and nRF24L01+ master-multiple-slave program [*][*][*]");
+
+    if (!EEPROM.begin(200)) {
+      Serial.println("Failed to initialise EEPROM");
+      Serial.println("Restarting...");
+      delay(1000);
+      ESP.restart();
+    }
+
+    byte counter = 0;
+    Serial.print("Waiting serial data in 10 seconds");
+    while(!Serial.available()) {
+      Serial.print('.');
+      delay(1000);
+      counter++;
+      if(counter>=10) break;
+    }
+
+    //strSSID#strPASS#Broker_Add#usernameMQTT#passwordMQTT#gatewayName>
+    //bryan-poenya#lalelilolu#broker.shiftr.io#patrol_system#patrol_system#ESP_Gateway_1>
+    
+    Serial.println("");
+    String outMessage = "";
+    while(Serial.available()>0){
+      char inChar = Serial.read();
+      if(inChar == '>'){
+        Serial.println("Recieve serial data");
+        Serial.println(outMessage);
+        Serial.println("Write to EEPROM");
+        EEPROM.writeString(0, outMessage);
+        EEPROM.commit();
+        Serial.println("Writing data success");
+        break;
+      }
+      else
+        outMessage.concat(inChar);
+    }
+
+    pinMode(LEDPIN,OUTPUT);
+    digitalWrite(LEDPIN,LOW);
+
+    Serial.println("Read data from EEPROM");
+    //EEPROM.writeString(0,"kingcrimson#lalilulelo#broker.shiftr.io#patrol_system#patrol_system#ESP_Gateway");
+    //EPROM.commit();
+    String readData = EEPROM.readString(0); 
+    Serial.println(readData);
+
+    strSSID = getValue(readData,'#',0); Serial.print("SSID     : "); Serial.println(strSSID);
+    strPASS = getValue(readData,'#',1); Serial.print("PASS SSID: ");Serial.println(strPASS);
+    strBrokerAdd = getValue(readData,'#',2); Serial.print("Broker Address: ");Serial.println(strBrokerAdd);
+    usernameMQTT = getValue(readData,'#',3); Serial.print("username      : ");Serial.println(usernameMQTT);
+    passwordMQTT = getValue(readData,'#',4); Serial.print("password      : ");Serial.println(passwordMQTT);
+    gatewayName = getValue(readData,'#',5); Serial.print("client name   : ");Serial.println(gatewayName);
+
+    Serial.println("Finish read\n");
+  
+    // put your setup code here, to run once:
+    strSSID.toCharArray(buff2,(strSSID.length()+1));
+    Serial.println(buff2);
+    strPASS.toCharArray(buff1,(strPASS.length()+1));
+    Serial.println(buff1);
+    
+    WiFi.begin(buff2, buff1);
+
+    // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+    // You need to set the IP address directly.
+    //client.begin("192.168.43.182", net); //mosquitto broker
+    strBrokerAdd.toCharArray(buff2,(strBrokerAdd.length()+1));
+    client.begin(buff2, net);
+    client.onMessage(mqttMessageReceived);
+    
+    mqttConnect();
+}
+
+void loop() {
+  // wait for any configuration payload from MQTT
+  client.loop();
+  if (!client.connected()) {
+    mqttConnect();
+  }
+}
+
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+
+void mqttConnect() {
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.print("WiFi connected");
+  Serial.print("\n" + strBrokerAdd + " connecting...");
+
+  gatewayName.toCharArray(buff2,gatewayName.length()+1);
+  usernameMQTT.toCharArray(buff3,usernameMQTT.length()+1);
+  passwordMQTT.toCharArray(buff4,passwordMQTT.length()+1);
+  
+  while (!client.connect(buff2, buff3, buff4)) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("\nbroker connected!");
+
+  LIST_NODENAME_TOPIC = "/config-gateway/" + gatewayName;
+  Serial.println(LIST_NODENAME_TOPIC);
+  client.subscribe(LIST_NODENAME_TOPIC);
+
+  for(int i = 0; i < addrAmount; i++) {
+    // subscribe to subtopic for receiving qrcode
+    String qrSubtopic = LIST_QR_TOPIC + strArrNodeAddr[i];
+    client.subscribe(qrSubtopic);
+    Serial.print("Subscribed to: "); Serial.println(qrSubtopic); 
+  }
+}
+
+void mqttMessageReceived(String &topic, String &payload) {
+  Serial.println("incoming: " + topic + " - " + payload);
+  if(topic == LIST_NODENAME_TOPIC){
+
+    //nodeAddr1#nodeAddr2#nodeAddr3#....#nodeAddr[n]
+    //NODE1#NODE2#NODE3#...#NODE[N]
+    
+    // count how many occurences of '#' to determine array size
+    addrAmount = 1;
+    for(int i = 0; i < payload.length(); i++) {
+      if(payload[i] == '#') addrAmount++;
+    }
+    Serial.print("Received address amount : "); Serial.println(addrAmount);
+    
+    for(int i = 0; i < addrAmount; i++) {
+      String tempNodeAddr = getValue(payload,'#',i); Serial.print("Node QR address : "); Serial.println(tempNodeAddr);
+
+      // store QR node address in array for index checking later
+      strArrNodeAddr[i] = tempNodeAddr;
+    }
+
+    // reconnect with new topic
+    client.disconnect();
+    mqttConnect();
+    
+    nrfConnect(); 
+  } 
+  
+  // check if topic is a /nodeqr subtopic
+  else if(topic.indexOf("/nodeqr/") > -1) {
+    // ensure we dont collect data from nodes faster than selected rate
+    currentTime = millis();
+    while (currentTime - lastSentTime <= SEND_RATE) {}
+
+    String targetQrNodeName = topic.substring(topic.lastIndexOf('/')+1);
+    
+    // send and receive sensor data from targeted node
+    callAndReceiveNodeData(targetQrNodeName, payload);
+    
+    lastSentTime = millis();
+  }
+}
+
+
+void nrfConnect() {
+  digitalWrite(LEDPIN,LOW);
+  Serial.println("Begin nRF24L01+ configuration...");
+  // begin radio object
+  radio.begin();
+    
+  // set power level of the radio
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // set RF datarate - lowest rate for longest range capability
+  radio.setDataRate(RF24_250KBPS);
+  
+  // set time between retries and max no. of retries
+  radio.setRetries(4, 5);
+  
+  // enable ack payload - each slave replies with sensor data using this feature
+  radio.enableAckPayload();
+
+  digitalWrite(LEDPIN,HIGH);
+  Serial.println("nRF24L01+ configured...");
+}
+
+/* Function: callAndReceiveNodeData
+ *    Make a radio call to each node in turn and retreive a message from each
+ */
+void callAndReceiveNodeData(String targetQrNodeName, String payload) {
+    Serial.println("[*] Master unit has successfully sent and received data ");
+    
+    byte byteArrTargetQrNodeName[targetQrNodeName.length()];
+    targetQrNodeName.getBytes(byteArrTargetQrNodeName, targetQrNodeName.length());
+    
+    radio.openWritingPipe(byteArrTargetQrNodeName);
+    radio.openReadingPipe(1, byteArrTargetQrNodeName);
+    
+    radio.stopListening();
+    
+    Serial.print("[*] Attempting to transmit data to node ");
+    Serial.println(targetQrNodeName);
+    Serial.print("[*] The payload being sent is: ");
+    Serial.println(payload);
+    
+    // boolean to indicate if radio.write() tx was successful
+    bool tx_sent;
+    tx_sent = radio.write(&payload, sizeof(payload));
+    
+    // if tx success - receive and read slave node ack reply
+    if (tx_sent) {
+        if (radio.isAckPayloadAvailable()) {
+            
+            int matchedIdx = 0;
+            for(matchedIdx; matchedIdx < addrAmount; matchedIdx++) {
+               if(targetQrNodeName == strArrNodeAddr[matchedIdx]) break;
+            }
+            // read ack payload and copy data to relevant remoteNodeData array
+            radio.read(&remoteNodeData[matchedIdx], sizeof(remoteNodeData[matchedIdx]));
+            
+            Serial.print("[+] Successfully received data from node: ");
+            Serial.print(targetQrNodeName + " with index of ");
+            Serial.println(matchedIdx);
+            Serial.print("  ---- The node count received was: ");
+            Serial.println(remoteNodeData[matchedIdx].messageCount);
+            Serial.print("  ---- The message was: ");
+            Serial.println(remoteNodeData[matchedIdx].message);
+        }
+    }
+    else {
+        Serial.print("[-] The transmission to the selected node failed.");
+    }
+
+    Serial.println("--------------------------------------------------------");
+}
---
+++ slave/slave.ino
@@ -0,0 +1,307 @@
+/*********************
+  | EEPROM
+*********************/
+#include "EEPROM.h"
+#define EEPROM_SIZE 128
+String strAddress;
+
+/*********************
+  | nRF24L01+
+*********************/
+#include <SPI.h>
+#include <RF24.h>
+#include <printf.h>
+
+// chip select and RF24 radio setup pins
+#define CE_PIN 17
+#define CSN_PIN 4
+RF24 radio(CE_PIN,CSN_PIN);
+
+// check how many times mesage has been received 
+int messageCount = 0;
+// String to store payload from master
+String dataFromMaster = "";
+
+struct payload {
+   String message;
+   int messageCount;
+};
+
+typedef struct payload Payload;
+
+/*********************
+  | SCREEN TFT ILI9225 176*220
+*********************/
+#include "SPI.h"
+#include "TFT_22_ILI9225.h"
+#include <../fonts/FreeSans9pt7b.h>
+#include <../fonts/FreeSans12pt7b.h>
+#define TFT_RST 26
+#define TFT_RS  25
+#define TFT_CLK 18
+#define TFT_SDI 23
+#define TFT_CS  5
+#define TFT_LED 0     // 0 if wired to +5V directly
+#define TFT_BRIGHTNESS 200 // Initial brightness of TFT backlight (optional)
+// Use hardware SPI (faster - on Uno: 13-SCK, 12-MISO, 11-MOSI)
+TFT_22_ILI9225 TFTscreen = TFT_22_ILI9225(TFT_RST, TFT_RS, TFT_CS, TFT_LED, TFT_BRIGHTNESS);
+int16_t x=0, y=0, width, height; //position;
+String strTextDisplay;
+
+/*********************
+  | QR CODE
+*********************/
+#include "qrcode.h"
+QRCode qrcode;
+uint8_t vers = 3;
+uint8_t pixel = 5;
+uint8_t offset_x = 15;
+uint8_t offset_y = 5;
+uint8_t borderWidth = 5;
+char dataCharArray[100];
+String strQrcode="";
+
+#define LEDPIN 2
+
+/*********************
+  | METHOD DEFINITIONS
+*********************/
+String getValue(String data, char separator, int index);
+void lcdStartup();
+void nrfConnect(byte nodeAddress[]);
+void radioCheckAndReply();
+void printQR(String strData);
+void updateNodeData();
+
+void switchToNRF() {
+  digitalWrite(CSN_PIN,HIGH);
+  digitalWrite(TFT_CS,HIGH);
+}
+
+void switchToLCD() {
+  digitalWrite(CSN_PIN,LOW);
+  digitalWrite(TFT_CS,LOW); 
+}
+
+void setup() {
+
+  // setup serial communications for basic program display
+  Serial.begin(115200);
+  Serial.println("[*][*][*] Beginning nRF24L01+ ack-payload slave device program [*][*][*]");
+
+  if (!EEPROM.begin(200)) {
+    Serial.println("Failed to initialise EEPROM");
+    Serial.println("Restarting...");
+    delay(1000);
+    ESP.restart();
+  }
+
+  byte counter = 0;
+  Serial.print("Waiting serial data in 10 seconds");
+  while(!Serial.available()) {
+    Serial.print('.');
+    delay(1000);
+    counter++;
+    if(counter>=10) break;
+  }
+
+  // strAddress>
+  // QR_Node>
+    
+  Serial.println("");
+  String outMessage = "";
+  while(Serial.available()>0){
+    char inChar = Serial.read();
+    outMessage.concat(inChar);
+    if(inChar == '>'){
+      Serial.println("Receive serial data");
+      Serial.println(outMessage);
+      Serial.println("Write to EEPROM");
+      EEPROM.writeString(0,outMessage);
+      EEPROM.commit();
+      Serial.println("Writing data success");
+      break;
+    }
+  }
+  
+  pinMode(LEDPIN,OUTPUT);
+  digitalWrite(LEDPIN,LOW);
+
+  Serial.println("Read data from EEPROM");
+  String readData = EEPROM.readString(0); 
+  Serial.println(readData);
+
+  strAddress = getValue(readData,'>',0); Serial.print("Node address  : "); Serial.println(strAddress);
+  
+  Serial.println("Finish read\n");
+
+  byte byteArrNodeAddress[strAddress.length()];
+  strAddress.getBytes(byteArrNodeAddress, strAddress.length());
+
+  lcdStartup();
+  nrfConnect(byteArrNodeAddress);
+
+  pinMode(CSN_PIN, OUTPUT);
+  pinMode(TFT_CS, OUTPUT);
+
+  switchToNRF();
+}
+
+
+/* Function: loop
+ *    main loop program for the slave node - repeats continuously during system operation
+ */
+void loop() {
+  radioCheckAndReply();
+}
+
+String getValue(String data, char separator, int index) {
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void lcdStartup() {
+  TFTscreen.begin();
+  TFTscreen.setOrientation(2);
+  TFTscreen.setBacklightBrightness(128);
+}
+
+void nrfConnect(byte nodeAddress[]) {
+  radio.begin();
+  
+  // set power level of the radio
+  radio.setPALevel(RF24_PA_LOW);
+
+  // set RF datarate
+  radio.setDataRate(RF24_250KBPS);
+
+  radio.openWritingPipe(nodeAddress);
+  radio.openReadingPipe(1, nodeAddress);     
+
+  radio.enableAckPayload();
+
+  Payload payloadReturn;
+  payloadReturn.message = "Connected!";
+  payloadReturn.messageCount = messageCount;
+  
+  radio.writeAckPayload(1, &payloadReturn, sizeof(payloadReturn));
+
+  // print radio config details to console
+  printf_begin();
+  radio.printDetails();
+
+  // start listening on radio
+  radio.startListening();
+
+  digitalWrite(LEDPIN,HIGH);
+}
+
+
+/* Function: radioCheckAndReply
+ *    sends the preloaded node data over the nrf24l01+ radio when
+ *    a message is received by the master
+ */
+void radioCheckAndReply() {
+    // check for radio message and send sensor data using auto-ack
+    if ( radio.available() ) {
+          radio.read( &dataFromMaster, sizeof(dataFromMaster) );
+          Serial.println("Received request from master - sending preloaded data.");
+          Serial.print("The received payload from the master was: ");
+          Serial.println(dataFromMaster);
+          Serial.println("--------------------------------------------------------");
+
+          printQR(dataFromMaster);
+          
+          // update the node count after sending ack payload - provides continually changing data
+          updateNodeData();
+    }
+    delay(1000);
+}
+
+void printQR(String strData) {
+  // Temporarily switch SPI bus to LCD 
+  switchToLCD();
+  
+  TFTscreen.clear();
+
+  if(strData!=""){
+    /**********************
+    | QR Code             |
+    **********************/
+    strData.toCharArray(dataCharArray, sizeof(dataCharArray));
+    // Print to TFT
+    uint8_t qrcodeData[qrcode_getBufferSize(3)];
+    qrcode_initText(&qrcode, qrcodeData, vers, 0, dataCharArray); // your text in last parameter, e.g. "Hello World"
+
+    //make border
+    TFTscreen.fillRectangle((offset_x - borderWidth), (offset_y - borderWidth), ((qrcode.size * pixel) + offset_x + borderWidth), offset_y, COLOR_WHITE); //square1 horizontal atas
+    TFTscreen.fillRectangle((offset_x - borderWidth), offset_y, offset_x, ((qrcode.size * pixel) + offset_y), COLOR_WHITE); //square2 vertical kiri
+    TFTscreen.fillRectangle((offset_x - borderWidth), ((qrcode.size * pixel) + offset_y), ((qrcode.size * pixel) + offset_x + borderWidth), ((qrcode.size * pixel) + offset_y + borderWidth), COLOR_WHITE); //square3 horizontal bawah
+    TFTscreen.fillRectangle(((qrcode.size * pixel) + offset_x), offset_y, ((qrcode.size * pixel) + offset_x + borderWidth), ((qrcode.size * pixel) + offset_y), COLOR_WHITE); //square4
+
+    for (uint8_t y = 0; y < qrcode.size; y++) { //vertical
+      for (uint8_t x = 0; x < qrcode.size; x++) { //horizontal
+        if (!qrcode_getModule(&qrcode, x, y)) {
+          uint16_t x1 = (x * pixel) + offset_x;
+          uint16_t y1 = (y * pixel) + offset_y;
+          uint16_t x2 = (x1 + pixel - 1);
+          uint16_t y2 = (y1 + pixel - 1);
+          TFTscreen.fillRectangle(x1, y1, x2, y2, COLOR_WHITE);
+        }
+      }
+    }
+  }
+
+  strTextDisplay = "Slave Node"; // Create string object
+  TFTscreen.setGFXFont(&FreeSans9pt7b);
+  TFTscreen.getGFXTextExtent(strTextDisplay, x, y, &width, &height); // Get string extents
+  x = 10;
+  y = 160 + height; // Set y position to string height plus shift down 10 pixels
+  TFTscreen.drawGFXText(x, y, strTextDisplay, COLOR_WHITE); // Print string
+
+  strTextDisplay = "UKDW"; // Create string object
+  TFTscreen.getGFXTextExtent(strTextDisplay, x, y, &width, &height); // Get string extents
+  x = 10;
+  y += height + 5; // Set y position to string height plus shift down 10 pixels
+  TFTscreen.drawGFXText(x, y, strTextDisplay, COLOR_ORANGE); // Print string
+
+  strTextDisplay = "Patrolee System"; // Create string object
+  //TFTscreen.setGFXFont(&FreeSans9pt7b);
+  TFTscreen.getGFXTextExtent(strTextDisplay, x, y, &width, &height); // Get string extents
+  x = 10;
+  y += height + 5; // Set y position to string height plus shift down 10 pixels
+  TFTscreen.drawGFXText(x, y, strTextDisplay, COLOR_CYAN); // Print string
+
+  // switch back to NRF
+  switchToNRF();
+}
+
+/* Function: updateNodeData
+ *    updates the count variable for the node and stores it in the nRF24L01+ radio
+ *    preloaded ack payload ready for sending on next received message
+ */
+void updateNodeData() {
+  // increment node count - reset to 1 if exceeds 500
+  if (messageCount < 500) {
+    messageCount++;
+  } else {
+    messageCount = 1;
+  }
+
+  Payload payloadReturn;
+  payloadReturn.message = "Received!";
+  payloadReturn.messageCount = messageCount;
+  
+  radio.writeAckPayload(1, &payloadReturn, sizeof(payloadReturn));
+}
